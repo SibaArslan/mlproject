4a5d83e5fb52b9b720bca94c8149c446986d2ce2
SibaArslan <sibaarsalan@gmail.com>
2026-09-09 12:17:49 +0300

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    if exc_tb is None:
+        return "Error occurred: {0}".format(str(error))
+
+    filename = exc_tb.tb_frame.f_code.co_filename
+    return (
+        "Error occurred in python script name [{0}] line number [{1}] "
+        "error message [{2}]"
+    ).format(filename, exc_tb.tb_lineno, str(error))
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
